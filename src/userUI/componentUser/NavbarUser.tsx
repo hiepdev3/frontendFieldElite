f174163104e53e1bdc431f4edf598938e84b76c4
Mr.Warren <hiepdevo@gmail.com>
2025-07-15 16:16:47 +0700

    s

--- src/userUI/componentUser/NavbarUser.tsx
+++ src/userUI/componentUser/NavbarUser.tsx
@@ -67,9 +67,9 @@ export default function NavbarUser({ cartItemCount = 0 }: { cartItemCount?: numb
                Sân 
 
               </a>
-              <a href="/promotions-user" className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 cursor-pointer transition-colors duration-200">
+              {/* <a href="/promotions-user" className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 cursor-pointer transition-colors duration-200">
                Khuyến Mãi
-              </a>
+              </a> */}
               {currentUser && (
                 <a href="/Match-Manage" className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 cursor-pointer transition-colors duration-200">
                   Tìm Đối
